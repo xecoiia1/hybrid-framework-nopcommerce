8e07e6c1f4ec7afa172b86cb1708d8c0a6ae1f8b
thanh5.nguyen <thanh5.nguyen@VN-MF10-NC100VB.LGE.NET>
2022-08-24 16:34:06 +0700

    Add

--- javaTester/javaBasic/Topic_01_Data_Type.java
+++ javaTester/javaBasic/Topic_01_Data_Type.java
@@ -1,5 +1,12 @@
 package javaBasic;
 
 public class Topic_01_Data_Type {
-
+	static int studentNumber;
+	static boolean Name;
+	
+	public static void main(String[] arg) {
+		System.out.println(studentNumber);
+		System.out.println(Name);
+		
+	}
 }
