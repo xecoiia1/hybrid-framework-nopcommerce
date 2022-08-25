a00a1afc29cf9495f1a9f5f2464d65970f523bbe
Thanh Nguyen <nguyendinhthanh27795@gmail.com>
2022-08-25 22:30:13 +0700

    Add

---
+++ javaTester/javaBasic/Topic_03_Compare.java
@@ -0,0 +1,12 @@
+package javaBasic;
+
+public class Topic_03_Compare {
+	static int studentNumber;
+	static boolean Name;
+	
+	public static void main(String[] arg) {
+		System.out.println(studentNumber);
+		System.out.println(Name);
+		//áds
+	}
+}
