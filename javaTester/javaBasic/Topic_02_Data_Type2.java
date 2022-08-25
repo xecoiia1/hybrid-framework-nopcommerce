19f77cd2a2ba849d527578abfda8a81d8d368391
thanh5.nguyen <thanh5.nguyen@VN-MF10-NC100VB.LGE.NET>
2022-08-25 17:12:48 +0700

    Add

---
+++ javaTester/javaBasic/Topic_02_Data_Type2.java
@@ -0,0 +1,12 @@
+package javaBasic;
+
+public class Topic_02_Data_Type2 {
+	static int studentNumber;
+	static boolean Name;
+	
+	public static void main(String[] arg) {
+		System.out.println(studentNumber);
+		System.out.println(Name);
+		
+	}
+}
