9896eb1d9adb87af0d74cf128f0b732610beff92
thanh5.nguyen <thanh5.nguyen@VN-MF10-NC100VB.LGE.NET>
2022-08-29 17:01:48 +0700

    Add

---
+++ javaTester/javaBasic/Topic_04_Switch_Case.java
@@ -0,0 +1,106 @@
+package javaBasic;
+
+import java.util.Scanner;
+
+public class Topic_04_Switch_Case {
+	
+	public static void main(String[] arg) {
+		//1
+//	Scanner inputText = new Scanner(System.in);
+//	
+//	System.out.println("input a : ");
+//	
+//	int a = inputText.nextInt();
+//	
+//	switch (a) {
+//		case 1: 
+//			System.out.println("One");
+//			break;
+//		case 2: 
+//			System.out.println("Two");
+//			break;
+//		case 3: 
+//			System.out.println("Three");
+//			break;
+//		case 4: 
+//			System.out.println("Four");
+//			break;
+//		case 5: 
+//			System.out.println("Five");
+//			break;
+//		case 6: 
+//			System.out.println("Six");
+//			break;
+//		case 7: 
+//			System.out.println("Seven");
+//			break;
+//		case 8: 
+//			System.out.println("Eight");
+//			break;
+//		case 9: 
+//			System.out.println("Nine");
+//			break;
+//		case 10: 
+//			System.out.println("Ten");
+//			break;
+//	}
+//		System.out.println("Wrong input");
+	
+		//2
+//		Scanner inputText = new Scanner(System.in);
+//		
+//		System.out.println("Input math: ");
+//		
+//		int a = inputText.nextInt();
+//		
+//		int b = inputText.nextInt();
+//	
+//		String c = inputText.next();
+//							
+//		switch(c) {
+//			case "+":
+//				System.out.println("ket qua: " + (a + b) );
+//				break;
+//			
+//			case "-":
+//				System.out.println("ket qua: " + (a - b) );
+//				break;
+//			
+//			case "*":
+//				System.out.println("ket qua: " + (a * b) );
+//				break;
+//			case "/":
+//				System.out.println("ket qua: " + (a / b) );
+//				break;
+//			
+//			default: 
+//				System.out.println("Nhap sai cong thuc");
+//				break;
+//		}
+		//3
+//		Scanner inputText = new Scanner(System.in);
+//		System.out.println("Input month: ");
+//		int a = inputText.nextInt();
+//		
+//		switch(a) {
+//			case 1: 
+//			case 3:
+//			case 5:
+//			case 7:
+//			case 8:
+//			case 10:
+//			case 12:
+//				System.out.println("Thang nay co 31 ngay");
+//				break;
+//			case 4:
+//			case 6:
+//			case 9:
+//			case 11:
+//				System.out.println("Thang nay co 30 ngay");
+//				break;
+//			case 2:
+//				System.out.println("Thang nay co 28/29 ngay");
+//				break;
+//		}
+	}
+}
---
+++ javaTester/javaBasic/Topic_05_For_ForEach.java
@@ -0,0 +1,45 @@
+package javaBasic;
+
+import java.util.Scanner;
+
+public class Topic_05_For_ForEach {
+	
+	public static void main(String[] arg) {
+//		//1
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("Input a: ");
+//		int a = scanner.nextInt();
+//		
+//		for ( int i = 0; i <= a; i++ ) {
+//			System.out.print(" " + i);
+//		}
+		//2
+//		Scanner scanner = new Scanner(System.in);
+//		int a = scanner.nextInt();
+//		int b = scanner.nextInt();
+//		
+//		if (a < b) {
+//		for (;a<=b;a++) {
+//			System.out.print(a + " ");
+//		}
+//		}else if(a > b) {
+//			for (;a>=b;a--) {
+//				System.out.print(a + " ");
+//			}
+//		}
+		
+		//3
+//		Scanner scanner = new Scanner(System.in);
+//		int a = scanner.nextInt();
+//		
+//		for (; a <=10; a++) {
+//			if (a%2==0) {
+//				System.out.println(a);
+//			}
+//		}
+		
+		//4
+
+			
+	}
+}
