b7d6ecc6fcef531d1b978541d466eda5b0018015
thanh5.nguyen <thanh5.nguyen@VN-MF10-NC100VB.LGE.NET>
2022-09-06 16:25:43 +0700

    Add

---
+++ javaTester/javaBasic/Topic_06_While_Do_While.java
@@ -0,0 +1,12 @@
+package javaBasic;
+
+public class Topic_06_While_Do_While {
+	static int studentNumber;
+	static boolean Name;
+	
+	public static void main(String[] arg) {
+		System.out.println(studentNumber);
+		System.out.println(Name);
+		
+	}
+}
