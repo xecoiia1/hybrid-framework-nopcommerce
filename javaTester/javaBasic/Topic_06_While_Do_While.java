1c5d22c53d10d43c8627c0b656a5327d1bdc3134
Thanh Nguyen <nguyendinhthanh27795@gmail.com>
2022-09-15 22:57:28 +0700

    Add

--- javaTester/javaBasic/Topic_06_While_Do_While.java
+++ javaTester/javaBasic/Topic_06_While_Do_While.java
@@ -1,12 +1,45 @@
 package javaBasic;
+import java.util.Scanner;
 
 public class Topic_06_While_Do_While {
 	static int studentNumber;
 	static boolean Name;
+	static Scanner scanner = new Scanner(System.in);
 	
 	public static void main(String[] arg) {
-		System.out.println(studentNumber);
-		System.out.println(Name);
 		
+//		int a = 1;
+//		int i = 1;
+//		
+//		while(a <5) {
+//			System.out.println(a);
+//			a++;
+//		}
+//		
+//		do {
+//			System.out.println(i);
+//			i++;
+//		} while (i < 5);
+		
+//		int a = scanner.nextInt();
+//		int b = scanner.nextInt();
+//		
+//		while (a < b) {
+//			if(a % 2 == 0 && a % 10 == 0) {
+//				System.out.println(a);
+//			}
+//			a++;
+//		}
+
+		int a = scanner.nextInt();
+		int q = 0 ;
+		
+		while (a > 0) {
+			if(a % 2 != 0 ) {
+				System.out.println(a);
+				q += a;
+			}
+			a--;
+		}
 	}
 }
