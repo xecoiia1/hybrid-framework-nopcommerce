324d06f932430cc60b96cb10d02fcacf3b0e7cf7
Thanh Nguyen <nguyendinhthanh27795@gmail.com>
2022-09-21 22:10:13 +0700

    Add Testcases basic

--- testcases/com/nopcommerce/user/User_01_Register_Login.java
+++ testcases/com/nopcommerce/user/User_01_Register_Login.java
@@ -2,14 +2,49 @@ package com.nopcommerce.user;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 
 public class User_01_Register_Login {
-  @Test
-  public void f() {
-  }
+  WebDriver driver;
+  String projectPath = System.getProperty("user.dir");
+
   @BeforeClass
   public void beforeClass() {
+	System.getProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+	driver = new FirefoxDriver();
+	
+	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	
+	driver.get("https://demo.nopcommerce.com/");
+  }
+  
+  @Test
+  public void TC_01_Register_Empty_Data() {
+  }
+  
+  @Test
+  public void TC_02_Register_Invalid_Email() {
+  }
+  
+  @Test
+  public void TC_03_Register_Success() {
+  }
+  
+  @Test
+  public void TC_04_Register_Existing_Email() {
+  }
+  
+  @Test
+  public void TC_05_Register_Password_Less_Than_6_Chars() {
+  }
+  
+  @Test
+  public void TC_06_Register_Invalid_Confirm_Password(){
   }
 
   @AfterClass
