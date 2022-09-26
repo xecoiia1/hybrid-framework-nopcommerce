d9329518e33f8774317e6924ad9148ee790e36bb
thanh5.nguyen <thanh5.nguyen@VN-MF10-NC100VB.LGE.NET>
2022-09-26 14:13:52 +0700

    Add/Delete

---
+++ testcases/com/nopcommerce/user/User_01_Register_Login_Apply_BasePage_1.java
@@ -0,0 +1,163 @@
+package com.nopcommerce.user;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+
+public class User_01_Register_Login_Apply_BasePage_1 {
+  WebDriver driver;
+  String projectPath = System.getProperty("user.dir");
+  String emailAdd;
+
+  @BeforeClass
+  public void beforeClass() {
+	System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+	
+	driver = new FirefoxDriver();
+	
+	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	
+	driver.get("https://demo.nopcommerce.com/");
+	
+	emailAdd = "abc" + generateRandom() + "@mail.com";
+  }
+  
+  @Test
+  public void TC_01_Register_Empty_Data() {
+	  driver.findElement(By.cssSelector("a.ico-register")).click();
+	  
+	  driver.findElement(By.cssSelector("button#register-button")).click();
+	  
+	  Assert.assertEquals(driver.findElement(By.cssSelector("span#FirstName-error")).getText(), "First name is required.");
+	  
+	  Assert.assertEquals(driver.findElement(By.cssSelector("span#LastName-error")).getText(), "Last name is required.");
+	  
+	  Assert.assertEquals(driver.findElement(By.cssSelector("span#Email-error")).getText(), "Email is required.");
+	  
+	  Assert.assertEquals(driver.findElement(By.cssSelector("span#Password-error")).getText(), "Password is required.");
+	  
+	  Assert.assertEquals(driver.findElement(By.cssSelector("span#ConfirmPassword-error")).getText(), "Password is required.");
+	  
+	  
+  }
+  
+  @Test
+  public void TC_02_Register_Invalid_Email() {
+	  driver.findElement(By.cssSelector("a.ico-register")).click();
+	  
+	  driver.findElement(By.cssSelector("input#FirstName")).sendKeys("Thanh");
+	  
+	  driver.findElement(By.cssSelector("input#LastName")).sendKeys("Nguyen");
+	  
+	  driver.findElement(By.cssSelector("input#Email")).sendKeys("blahbla");
+	  
+	  driver.findElement(By.cssSelector("input#Password")).sendKeys("123456");
+	  
+	  driver.findElement(By.cssSelector("input#ConfirmPassword")).sendKeys("123456");
+	  
+	  driver.findElement(By.cssSelector("button#register-button")).click();
+	  
+	  Assert.assertEquals(driver.findElement(By.cssSelector("span#Email-error")).getText(), "Wrong email");
+  }
+  
+  @Test
+  public void TC_03_Register_Success() {
+	  driver.findElement(By.cssSelector("a.ico-register")).click();
+	  
+	  driver.findElement(By.cssSelector("input#FirstName")).sendKeys("Thanh");
+	  
+	  driver.findElement(By.cssSelector("input#LastName")).sendKeys("Nguyen");
+	  
+	  driver.findElement(By.cssSelector("input#Email")).sendKeys(emailAdd);
+	  
+	  driver.findElement(By.cssSelector("input#Password")).sendKeys("123456");
+	  
+	  driver.findElement(By.cssSelector("input#ConfirmPassword")).sendKeys("123456");
+	  
+	  driver.findElement(By.cssSelector("button#register-button")).click();
+	  
+	  Assert.assertEquals(driver.findElement(By.cssSelector("div.result")).getText(), "Your registration completed");
+	  
+	  driver.findElement(By.cssSelector("a.ico-logout")).click();
+  }
+  
+  @Test
+  public void TC_04_Register_Existing_Email() {
+	  driver.findElement(By.cssSelector("a.ico-register")).click();
+	  
+	  driver.findElement(By.cssSelector("input#FirstName")).sendKeys("Thanh");
+	  
+	  driver.findElement(By.cssSelector("input#LastName")).sendKeys("Nguyen");
+	  
+	  driver.findElement(By.cssSelector("input#Email")).sendKeys(emailAdd);
+	  
+	  driver.findElement(By.cssSelector("input#Password")).sendKeys("123456");
+	  
+	  driver.findElement(By.cssSelector("input#ConfirmPassword")).sendKeys("123456");
+	  
+	  driver.findElement(By.cssSelector("button#register-button")).click();
+	  
+	  Assert.assertEquals(driver.findElement(By.cssSelector("div.validation-summary-errors")).getText(), "The specified email already exists");
+  }
+  
+  @Test
+  public void TC_05_Register_Password_Less_Than_6_Chars() {
+	  driver.findElement(By.cssSelector("a.ico-register")).click();
+	  
+	  driver.findElement(By.cssSelector("input#FirstName")).sendKeys("Thanh");
+	  
+	  driver.findElement(By.cssSelector("input#LastName")).sendKeys("Nguyen");
+	  
+	  driver.findElement(By.cssSelector("input#Email")).sendKeys(emailAdd);
+	  
+	  driver.findElement(By.cssSelector("input#Password")).sendKeys("123");
+	  
+	  driver.findElement(By.cssSelector("input#ConfirmPassword")).sendKeys("123");
+	  
+	  driver.findElement(By.cssSelector("button#register-button")).click();
+	  
+	  Assert.assertEquals(driver.findElement(By.cssSelector("span#Password-error")).getText(), "Password must meet the following rules:\nmust have at least 6 characters");
+	  
+  }
+  
+  @Test
+  public void TC_06_Register_Invalid_Confirm_Password(){
+	  driver.findElement(By.cssSelector("a.ico-register")).click();
+	  
+	  driver.findElement(By.cssSelector("input#FirstName")).sendKeys("Thanh");
+	  
+	  driver.findElement(By.cssSelector("input#LastName")).sendKeys("Nguyen");
+	  
+	  driver.findElement(By.cssSelector("input#Email")).sendKeys(emailAdd);
+	  
+	  driver.findElement(By.cssSelector("input#Password")).sendKeys("123456");
+	  
+	  driver.findElement(By.cssSelector("input#ConfirmPassword")).sendKeys("123567");
+	  
+	  driver.findElement(By.cssSelector("button#register-button")).click();
+	  
+	  Assert.assertEquals(driver.findElement(By.cssSelector("span#ConfirmPassword-error")).getText(), "The password and confirmation password do not match.");
+	  
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  driver.quit();
+  }
+  
+  public int generateRandom() {
+	  Random rand = new Random();
+	  return rand.nextInt(9999);
+  }
+//  //Jquery check textcontent
+//  var element = $$("div.validation-summary-errors")[0];
+//  element.textContent;
+}
