af3aef1321562c944aa66ddbb7183d3551f3beec
thanh5.nguyen <thanh5.nguyen@VN-MF10-NC100VB.LGE.NET>
2022-10-28 15:30:19 +0700

    Add

--- actions/exception/BrowserNotSupport.java
+++ actions/exception/BrowserNotSupport.java
@@ -1,6 +1,6 @@
 package exception;
 
-public class BrowserNotSupport extends Exception {
+public class BrowserNotSupport extends IllegalArgumentException {
 
 	private static final long serialVersionUID = 1L;
 	
