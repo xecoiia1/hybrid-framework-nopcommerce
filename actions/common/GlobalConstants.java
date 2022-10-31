8498d188e8cd2a40ba21623f53b748fc58157fe2
thanh5.nguyen <thanh5.nguyen@VN-MF10-NC100VB.LGE.NET>
2022-10-31 14:07:38 +0700

    Add

--- actions/common/GlobalConstants.java
+++ actions/common/GlobalConstants.java
@@ -1,6 +1,12 @@
 package common;
 
+import java.io.File;
+
 public class GlobalConstants {
 	public static final String PORTAL_PAGE_URL = "https://demo.nopcommerce.com/";
 	public static final String ADMIN_PAGE_URL = "https://admin-demo.nopcommerce.com/login?ReturnUrl=%2Fadmin%2F";
+	public static final String PROJECT_PATH = System.getProperty("user.dir");
+	
+	//Tạo đường dẫn cho cả Window/Linux/Mac
+	public static final String UPLOAD_FOLDER = PROJECT_PATH + File.separator + "";
 }
