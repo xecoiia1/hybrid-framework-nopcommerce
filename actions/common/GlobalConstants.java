e6c7cfbe163bb7312064f640764608a42db67a9b
thanh5.nguyen <thanh5.nguyen@VN-MF10-NC100VB.LGE.NET>
2022-10-31 14:22:14 +0700

    Add

--- actions/common/GlobalConstants.java
+++ actions/common/GlobalConstants.java
@@ -9,4 +9,7 @@ public class GlobalConstants {
 	
 	//Tạo đường dẫn cho cả Window/Linux/Mac
 	public static final String UPLOAD_FOLDER = PROJECT_PATH + File.separator + "";
+	public static final String DOWNLOAD_FOLDER = PROJECT_PATH + File.separator + "";
+	public static final String BROWSER_LOG = PROJECT_PATH + File.separator + "browserLogs";
+	public static final String DRAG_DROP_HTML = PROJECT_PATH + File.separator + "";
 }
