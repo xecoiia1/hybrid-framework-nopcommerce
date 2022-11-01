ee9dbc408b55dc1d4b25a0db25d5ae3805381ca1
thanh5.nguyen <thanh5.nguyen@VN-MF10-NC100VB.LGE.NET>
2022-11-01 16:41:03 +0700

    Add

---
+++ interfaces/pageUI/Jquery/HomePageUI.java
@@ -0,0 +1,6 @@
+package pageUI.Jquery;
+
+public class HomePageUI {
+	public static final String PAGINATION_PAGE_BY_NUMBER = "xpath=//li[@class='qgrd-pagination-page']/a[text()='%s']";
+	public static final String HEADER_TEXTBOX_BY_LABEL = "xpath=//div[@class='qgrd-header-text' and text()='%s']/parent::div/following-sibling::input";
+}
--- testcases/com/jquery/datatable/Level_10_DataTable_DataGrid.java
+++ testcases/com/jquery/datatable/Level_10_DataTable_DataGrid.java
@@ -11,6 +11,8 @@ import common.BaseTest;
 import common.PageGeneratorManager;
 import commonJquery.BaseTestJquery;
 import exception.BrowserNotSupport;
+import pageObjJquery.HomePageObj;
+import pageObjJquery.PageGeneratorManagerJQuery;
 import pageObject.nopCommerce.user.UserAddressPageObj;
 import pageObject.nopCommerce.user.UserCustomerInforPageObj;
 import pageObject.nopCommerce.user.UserHomePageObj;
@@ -20,21 +22,29 @@ import pageObject.nopCommerce.user.UserRegisterPageObj;
 import pageObject.nopCommerce.user.UserRewardPointPageObj;
 
 public class Level_10_DataTable_DataGrid extends BaseTestJquery  {
+	HomePageObj homePage;
 
 	@Parameters({"browser","url"})
 	@BeforeClass
 	public void beforeClass(String browserName, String appUrl) throws BrowserNotSupport {
 	driver = getBrowserDriver(browserName, appUrl);	
+	homePage = PageGeneratorManagerJQuery.getHomePage(driver);
   }
   
   @Test
-  public void Table_01() {
+  public void Table_01_Paging() {
+		homePage.openPagingPageByPageNumber("10");
 		
+		homePage.openPagingPageByPageNumber("15");
+		
+		homePage.openPagingPageByPageNumber("18");
+		
+		homePage.openPagingPageByPageNumber("19");
   }
     
   @Test
-  public void Table_02() {
-	 
+  public void Table_02_Enter_Header_Textbox() {
+	  	homePage.enterToHeaderTextboxByLabel("Country", "Argentina");
   }
   
   @AfterClass
