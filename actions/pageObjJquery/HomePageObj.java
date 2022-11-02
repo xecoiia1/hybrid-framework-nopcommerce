cb18d450c334aa9aecce04c2d6874a65effd6907
thanh5.nguyen <thanh5.nguyen@VN-MF10-NC100VB.LGE.NET>
2022-11-02 16:54:55 +0700

    Add

--- actions/pageObjJquery/HomePageObj.java
+++ actions/pageObjJquery/HomePageObj.java
@@ -1,8 +1,10 @@
 package pageObjJquery;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -37,15 +39,18 @@ public class HomePageObj extends BasePageJquery {
 		return isElementDisplay(driver, HomePageUI.PAGINATION_PAGE_ACTIVED_BY_NUMBER, pageNumber);
 	}
 
-	public List getValueEachRowAtAllPage() {
+	public List<String> getValueEachRowAtAllPage() {
 		int totalPage = getElementSize(driver, HomePageUI.TOTAL_PAGINATION);
 		
 		List<String> allRowValueAllPage = new ArrayList<String>();
 		
+		//Dùng Set thì khi lưu vào sẽ chỉ lưu 1 giá trị, không lưu trùng
+		Set<String> allRowValueUniqueAllPage = new HashSet<String>();
+		
 		//Duyệt qua tất cả page number
 		for (int index = 1; index <= totalPage; index++) {
 			clickToElement(driver, HomePageUI.PAGINATION__BY_INDEX, String.valueOf(index));
-			sleepInSecond(1);
+			//sleepInSecond(1);
 			
 			//Get text của all row mỗi page và đưa vào array List
 			List<WebElement> allRowValueEachPage = getListWebElement(driver, HomePageUI.ALL_ROW_EACH_PAGE);
