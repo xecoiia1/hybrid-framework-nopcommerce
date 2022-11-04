1cc5a9feb97388dff138f817f25be6c50a1896bf
thanh5.nguyen <thanh5.nguyen@VN-MF10-NC100VB.LGE.NET>
2022-11-04 14:56:19 +0700

    Add

---
+++ actions/pageObjUploadFiles/HomePageObj.java
@@ -0,0 +1,25 @@
+package pageObjUploadFiles;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import common.BasePage;
+import commonJquery.BasePageJquery;
+import pageUI.Jquery.HomePageUI;
+
+public class HomePageObj extends BasePageJquery {
+	WebDriver driver;
+
+	public HomePageObj(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	
+}
---
+++ actions/pageObjUploadFiles/PageGeneratorManagerJQuery.java
@@ -0,0 +1,10 @@
+package pageObjUploadFiles;
+
+import org.openqa.selenium.WebDriver;
+
+public class PageGeneratorManagerJQuery {
+
+	public static HomePageObj getHomePage(WebDriver driver) {
+			return new HomePageObj(driver);
+	}
+}
