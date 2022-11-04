cf5ace2abfd6c87775235aeeed755be26ce9e272
thanh5.nguyen <thanh5.nguyen@VN-MF10-NC100VB.LGE.NET>
2022-11-04 14:53:28 +0700

    Add

---
+++ testcases/com/jquery/datatable/Level_11_Upload_Files.java
@@ -0,0 +1,58 @@
+package com.jquery.datatable;
+
+import static org.testng.Assert.assertTrue;
+
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import common.BaseTest;
+import common.PageGeneratorManager;
+import commonJquery.BaseTestJquery;
+import exception.BrowserNotSupport;
+import pageObjJquery.HomePageObj;
+import pageObjJquery.PageGeneratorManagerJQuery;
+import pageObject.nopCommerce.user.UserAddressPageObj;
+import pageObject.nopCommerce.user.UserCustomerInforPageObj;
+import pageObject.nopCommerce.user.UserHomePageObj;
+import pageObject.nopCommerce.user.UserLoginPageObj;
+import pageObject.nopCommerce.user.UserMyProductReviewPageObj;
+import pageObject.nopCommerce.user.UserRegisterPageObj;
+import pageObject.nopCommerce.user.UserRewardPointPageObj;
+import pageUI.Jquery.HomePageUI;
+
+public class Level_11_Upload_Files extends BaseTestJquery  {
+
+	@Parameters({"browser","url"})
+	@BeforeClass
+	public void beforeClass(String browserName, String appUrl) throws BrowserNotSupport {
+	driver = getBrowserDriver(browserName, appUrl);	
+  }
+  
+  @Test
+  public void Upload_01_One_File_Per_Time() {
+		
+  }
+  
+  @Test
+  public void Upload_02_Multi_Files_Per_Time() {
+		
+  }
+  
+  
+  @AfterClass
+  public void afterClass() {
+	  driver.quit();
+  }
+  
+//  //Jquery check textcontent
+//  var element = $$("div.validation-summary-errors")[0];
+//  element.textContent;
+ private WebDriver driver;
+
+}
