d3fc972b6ff2c28607342a2fc7ef48c8e396c9f8
Thanh Nguyen <nguyendinhthanh27795@gmail.com>
2022-11-10 23:16:48 +0700

    Add

---
+++ testcases/com/facebook/register/Level_13_Element_Undisplayed.java
@@ -0,0 +1,67 @@
+package com.facebook.register;
+
+import static org.testng.Assert.assertTrue;
+
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import common.BaseTest;
+import common.PageGeneratorManager;
+import commonJquery.BaseTestJquery;
+import exception.BrowserNotSupport;
+import pageObjJquery.HomePageObj;
+import pageObjJquery.PageGeneratorManagerJQuery;
+import pageObject.nopCommerce.user.UserAddressPageObj;
+import pageObject.nopCommerce.user.UserCustomerInforPageObj;
+import pageObject.nopCommerce.user.UserHomePageObj;
+import pageObject.nopCommerce.user.UserLoginPageObj;
+import pageObject.nopCommerce.user.UserMyProductReviewPageObj;
+import pageObject.nopCommerce.user.UserRegisterPageObj;
+import pageObject.nopCommerce.user.UserRewardPointPageObj;
+import pageUI.Jquery.HomePageUI;
+
+public class Level_13_Element_Undisplayed extends BaseTestJquery  {
+	
+
+	@Parameters({"browser","url"})
+	@BeforeClass
+	public void beforeClass(String browserName, String appUrl) throws BrowserNotSupport {
+	driver = getBrowserDriver(browserName, appUrl);
+	
+	
+  }
+  
+  @Test
+  public void TC_01_Verify_Element_Displayed() {
+	  
+  }
+  
+  @Test
+  public void TC_02_Verify_Element_Undisplayed_In_DOM() {
+
+  }
+  
+  @Test
+  public void TC_03_Verify_Element_Undisplayed_Not_In_DOM() {
+
+  }
+  
+  
+  @AfterClass
+  public void afterClass() {
+	  driver.quit();
+  }
+  
+//  //Jquery check textcontent
+//  var element = $$("div.validation-summary-errors")[0];
+//  element.textContent;
+ private WebDriver driver;
+
+
+}
